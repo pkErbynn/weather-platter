1b73d4e01d91f40954abaeb0bcca12df37e13b67
pkErbynn <john.erbynn@gmail.com>
2019-11-30 18:58:49 +0000

    json serialization expore

---
+++ src/main/java/io/turntabl/gsonSer/MapSer.java
@@ -0,0 +1,27 @@
+package io.turntabl.gsonSer;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapSer {
+    public static void main(String[] args) {
+        Map<String, String> myMap = new HashMap<String, String>();
+        myMap.put("one","hello");
+        myMap.put("two","world");
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(myMap);
+
+        System.out.println("json: " + json);
+
+        Type typeOfHashMap = new TypeToken<Map<String, String>>() {}.getType();
+        Map<String, String> newMap = gson.fromJson(json, typeOfHashMap); // This type must match TypeToken
+        System.out.println(newMap.get("one"));
+        System.out.println(newMap.get("two"));
+    }
+}
---
+++ src/main/java/io/turntabl/gsonSer/PojoToJson.java
@@ -0,0 +1,63 @@
+package io.turntabl.gsonSer;
+/*
+* converting Java object to JSON document using Google's Java library called Gson.
+* This library allows you to convert both Java object to JSON String and a JSON document to Java object.
+*  Serialization in the context of Gson means mapping a Java object to its JSON representation
+*
+* How?
+* add the dependency in gradle / pom.xml file.
+* Create the object to be converted into JSON
+* Create the object of Gson, helps to convert Java object to Gson
+* Call Gson.toJSon(object) to convert the object into JSON String.
+* */
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class PojoToJson {
+    private String fname;
+    private String lname;
+    private int age;
+    private String phone;
+    private String city;
+
+    public PojoToJson(String fname, String lname, int age, String phone, String city) {
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.phone = phone;
+        this.city = city;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    @Override
+    public String toString() {
+        return "PojoToJson{" +
+                "fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", age=" + age +
+                ", phone=" + phone +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
+
+
+class Run{
+    public static void main(String[] args) {
+        PojoToJson pojo = new PojoToJson("John", "Erbynn", 24, "+233-55-347-2114", null);
+
+        // Json Serialization
+        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+        String gsonPojo = gson.toJson(pojo);
+        System.out.println(gsonPojo);
+
+        // Json To Object
+        PojoToJson backToPojo = gson.fromJson(gsonPojo, PojoToJson.class);
+        System.out.println(backToPojo);
+        System.out.println("fname: " + backToPojo.getFname());
+    }
+}
